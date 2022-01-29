7284c308fe139c7adc78c1b3dc6c7e09fd23f07d
Elvaleryn <eraykaya121@gmail.com>
2022-01-29 15:16:43 +0300

    Contracts are added

---
+++ packages/core/src/contracts/erc20.ts
@@ -0,0 +1,27 @@
+import { Contract } from 'ethers';
+import ABI from '../abis/ERC20.json';
+import * as Types from '../../types/abis';
+import { BaseContract } from './base-contract';
+
+export class Erc20 extends BaseContract {
+	public contract!: Types.ERC20;
+
+	/**
+	 * @setContract
+	 *
+	 * Creates contract instance with given address
+	 */
+	setContract(): this {
+		if (!this.address || !this.provider) {
+			throw new Error('Please provide an address and network provider!');
+		}
+
+		this.contract = new Contract(
+			this.address,
+			ABI,
+			this.provider
+		) as any as Types.ERC20;
+
+		return this;
+	}
+}
